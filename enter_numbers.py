72a8e395bc98487e028401957b6060742855bc8a
CesarMartinez23 <oliver-cesar@hotmail.com>
2022-08-21 20:05:02 -0600

    Exercise Enter Numbers With Comments

--- enter_numbers.py
+++ enter_numbers.py
@@ -1,8 +1,15 @@
 
+# Creacion de una funcion para ingresar numeros y mientras este no sea un 0 (cero), se agregaran a una lista, cuando se ingrese el 0 (cero), se retornara la suma total de los numeros almacenados en la lista.
 def enterNumbers():
+    
+    # Se crea una lista vacia.
     numbers = []
+    
+    # Mientras el usuario no ingrese un 0 (cero), se ingresaran numeros a la lista.
     while True:
         number = int(input("Enter a number: "))
+        
+        # Si el usuario ingresa un 0 (cero), se retornara la suma total de los numeros almacenados en la lista.
         if(number == 0):
             break
         else:
